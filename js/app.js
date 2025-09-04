a35795fc9eae1f6c2a96cf49f671aa8214b0c91b
Fábio Dantas <fabio.tot.brisa@gmail.com>
2025-09-04 00:50:48 -0300

    Projeto concluído

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//Variaveis globais
+let inferior = 400;
+let superior = 200;
+let pista = 100;
+
+function comprar(){
+    //Recuperando dados dos elementos
+    let quantidade = document.getElementById('qtd').value;
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+
+    //Condicionais e calculo da quantidade de ingressos disponíveis
+    if (tipoIngresso == 'inferior'){
+        if ((inferior - quantidade) >= 0){
+            inferior = inferior - quantidade;
+        } else {
+           alert('Quantidade indisponível para INFERIOR.');
+           inferior = inferior;
+        }
+    } else {
+            if (tipoIngresso == 'superior') {
+                if ((superior - quantidade) >= 0){
+                } else {
+                    alert('Quantidade indisponível para SUPERIOR.');    
+                }      
+        } else {
+            if (pista - quantidade >= 0){
+                pista = pista - quantidade;
+            } else {
+                alert('Quantidade indisponível para PISTA.');             
+            }
+        }
+    }
+
+    //Mostrar quantidade atualizada ao usuário:
+    quantidadeRestante = document.getElementById('qtd-geral');
+    quantidadeRestante.innerHTML = `<li>Pista<span id="qtd-pista">${pista}</span></li>
+    <li>Cadeira superior<span id="qtd-superior">${superior}</span></li>
+    <li>Cadeira inferior<span id="qtd-inferior">${inferior}</span></li> `
+
+    // e geração de alertas
+
+}
